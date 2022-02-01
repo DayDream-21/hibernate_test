2aa0e77306cf390ceb6db6a57876a656f96ecd2d
DayDream-21 <vova13158@gmail.com>
2022-02-01 16:22:50 +0300

    Catalog class
    Delete equals and hashcode methods

--- src/main/java/com/slavamashkov/Catalog.java
+++ src/main/java/com/slavamashkov/Catalog.java
@@ -1,10 +1,7 @@
 package com.slavamashkov;
 
 import lombok.*;
-import org.hibernate.Hibernate;
-
 import javax.persistence.*;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -22,17 +19,4 @@ public class Catalog {
     @NonNull
     @Column(name = "title")
     private String title;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Catalog catalog = (Catalog) o;
-        return id != null && Objects.equals(id, catalog.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
