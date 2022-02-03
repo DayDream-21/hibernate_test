765bda93560c45b0298440bc41d2f77851edb1a5
DayDream-21 <vova13158@gmail.com>
2022-02-03 19:58:51 +0300

    Create methods for working with the database

--- src/main/java/com/slavamashkov/shop/ShopMain.java
+++ src/main/java/com/slavamashkov/shop/ShopMain.java
@@ -4,21 +4,145 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+
 public class ShopMain {
     public static void main(String[] args) {
+
+    }
+
+    private static void showProductsByCustomerName(String customerName) {
         try (SessionFactory sessionFactory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Customer.class)
-                .addAnnotatedClass(Product.class)
+                .configure("hibernate_shop.cfg.xml")
                 .buildSessionFactory();
              Session session = sessionFactory.getCurrentSession()) {
 
             session.beginTransaction();
 
-            Customer customer = session.get(Customer.class, 3);
-            Product product = session.get(Product.class, 2);
+            Customer customer = (Customer) session
+                    .createQuery("from Customer customer where customer.name = :name")
+                    .setParameter("name", customerName)
+                    .getSingleResult();
 
-            customer.getProducts().add(product);
+            List<Order> orders = customer.getOrders();
+            System.out.println("Products for " + customer.getName() + ": ");
+            orders.forEach(order -> System.out.println(order.getProduct().toString()));
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void showCustomersByProductName(String productName) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_shop.cfg.xml")
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Product product = (Product) session
+                    .createQuery("from Product product where product.name = :name")
+                    .setParameter("name", productName)
+                    .getSingleResult();
+
+            List<Order> orders = product.getOrders();
+            System.out.println("Customers for " + product.getName() + ": ");
+            orders.forEach(order -> System.out.println(order.getCustomer().toString()));
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void addCustomer(String customerName) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_shop.cfg.xml")
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Customer customer = new Customer(customerName);
+            session.save(customer);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void addProduct(String productName, BigDecimal price) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_shop.cfg.xml")
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Product product = new Product(productName, price);
+            session.save(product);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void removeCustomerByCustomerName(String customerName) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_shop.cfg.xml")
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Customer customer = (Customer) session
+                    .createQuery("from Customer customer where customer.name = :name")
+                    .setParameter("name", customerName)
+                    .getSingleResult();
+            session.delete(customer);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void removeProductByProductName(String productName) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_shop.cfg.xml")
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Product product = (Product) session
+                    .createQuery("from Product product where product.name = :name")
+                    .setParameter("name", productName)
+                    .getSingleResult();
+            session.delete(product);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private static void addProductToCustomerByName(String productName, String customerName) {
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate_shop.cfg.xml")
+                .buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            Customer customer = (Customer) session
+                    .createQuery("from Customer customer where customer.name = :name")
+                    .setParameter("name", customerName)
+                    .getSingleResult();
+
+            Product product = (Product) session
+                    .createQuery("from Product product where product.name = :name")
+                    .setParameter("name", productName)
+                    .getSingleResult();
+
+            Order order = new Order(new OrderKey(customer.getId(), product.getId()));
+            order.setPrice(product.getPrice());
+
+            session.save(order);
 
             session.getTransaction().commit();
         }
